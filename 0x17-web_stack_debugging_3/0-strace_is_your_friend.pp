2227c03fc9df7df6bae6994f94c7e8a62270c03c
Teresiah6 <wangariteresiah6@gmail.com>
2023-05-15 00:58:15 +0300

    starting over

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,1 @@
-# Fix Apache 500 error, automate using puppet.
-exec { 'attach_strace':
-  command => '/usr/bin/strace -p $(pidof apache2) -s 1024 -o /var/log/apache.strace',
-  onlyif  => '/usr/bin/pgrep apache2 > /dev/null',
-refreshonly => true,
-}
-
+# Fix Apache 500 error, automate using puppet
